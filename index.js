ff63be4b704c7880421e63099fa9fc651e99608a
Kyli Archibald <kyliarchibald@Kylis-MacBook-Pro.local>
2023-01-21 00:08:29 -0600

    code

---
+++ index.js
@@ -0,0 +1,32 @@
+const quizData = [
+    {question: "Commonly used data types DO NOT Include",
+a: "strings",
+b: "boolean",
+c: "alerts",
+d: "numbers",
+correct:"b",
+}
+]
+
+var quiz= document.getElementById("quiz");
+var answer= document.getElementById("answer");
+var questionEl= document.getElementById("question");
+var a_text= document.getElementById("a-text");
+var b_text= document.getElementById("b-text");
+var c_text= document.getElementById("c-text");
+var d_text= document.getElementById("d-text");
+var startBtn = document.getElementById("start");
+
+// let currenQquiz = 0
+// let score = 0
+
+// startQuiz()
+// function startQuiz() {
+//  quiz.innerHTML = currenQquiz.quiz
+//  a_text.innerHTML = currenQquiz.a
+//  b_text.innerHTML = currenQquiz.b
+//  c_text.innerHTML = currenQquiz.c
+//  d_text.innerHTML = currenQquiz.d
+// }
+
+
